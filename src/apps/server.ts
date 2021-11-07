61cec00af7caea9aaa0b129e1681338974f2ad21
Vladimir Greenberg <xenus90@gmail.com>
2021-11-07 21:20:51 +0200

    added dynamic heroku port

--- src/apps/server.ts
+++ src/apps/server.ts
@@ -5,7 +5,7 @@ import MainRouter from '../routers';
 
 export default class Server {
     static async start() {
-        const port = config.port;
+        const port = process.env.PORT || config.port;
         const app = express();
         app.use(middlewares());
         app.use('/', MainRouter.generateRouter());
